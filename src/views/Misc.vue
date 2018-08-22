a1a73f9650c870944675ecb84c30ec4b574d388a
Zhangyuan Nie <zyntuz@gmail.com>
2018-08-21 22:57:28 -0400

    bump version to 0.5.0

--- src/views/Misc.vue
+++ src/views/Misc.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="misc">
-    <h1>{{ $t('title') }}</h1>
+    <h1>{{ $t('title') }} 0.5.0</h1>
     <locale-changer></locale-changer>
+    <h3 style="color:red">{{ $t('important') }}</h3>
     <p>{{ $t('repo') }}</p>
     <a href="https://github.com/zyntuz/era-plus">https://github.com/zyntuz/era-plus</a>
     <p><button @click="clearStorage">{{ $t('clear') }}</button>{{ $t('clearDesc') }} </p>
-    <p>{{ $t('appVer') }}0.4.0<br>{{ $t('dataVer') }}2018-07-31</p>
+    <p>{{ $t('dataVer') }}2018-07-31</p>
     <h3>{{ $t('buildDesc') }}</h3>
     <p>{{ $t('computer') }}</p>
     <p>{{ $t('warning') }}</p>
@@ -43,26 +44,28 @@ export default {
 <i18n>
 {
   "en": {
-    "title": "Era+ Finder",
-    "repo": "Source code is on GitHub. Contributions are welcomed. This app is provided without warranty of any kind",
+    "title": "Era+",
+    "repo": "Source code is on GitHub. Contributions are welcomed. This app is provided without warranty of any kind.",
     "clear": "Clear saved data",
     "clearDesc": " You may need to click after update. You can also try this button if any bug occurs.",
     "appVer": "App version: ",
     "dataVer": "Database version: ",
     "buildDesc": "Build page description",
-    "computer": "Mobile support is still in beta.",
-    "warning": "Note: Energy, shield, armor and damage do not takes buffs into account. Module only takes one slot regardless of size."
+    "computer": "Mobile support is somehow usable.",
+    "warning": "Note: Energy, shield, armor and damage do not takes buffs into account. Module only takes one slot regardless of size.",
+    "important": "This page is no longer updated. Contributions are welcomed."
   },
   "zh": {
-    "title": "Era+ 查询器",
+    "title": "Era+",
     "repo": "锌兔制作。源代码在 GitHub 上公开，欢迎贡献。一切计算、查询结果本人不保证准确率也不负责（滑稽）。",
     "clear": "清除本页面缓存",
     "clearDesc": " 在大更新后，可能需要按一下并刷新页面才能正常使用。出 bug 或者查询器被玩坏了也可以试试这个按钮。",
     "appVer": "应用版本：",
     "dataVer": "数据库版本：",
     "buildDesc": "装配页面说明",
-    "computer": "移动端配船任然在 Beta 阶段",
-    "warning": "注意：能源、护盾、装甲和伤害未计算技能和船体加成。组件不管大小都只占1格。"
+    "computer": "移动端配船勉强可以用",
+    "warning": "注意：能源、护盾、装甲和伤害未计算技能和船体加成。组件不管大小都只占1格。",
+    "important": "本页面不再更新。欢迎贡献。"
   }
 }
 </i18n>
