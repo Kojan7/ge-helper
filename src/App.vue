861b1ce4a0bdd0f17eb407dfdfa8ce00ae7e0a80
Zhangyuan Nie <zyntuz@gmail.com>
2018-10-07 00:38:48 -0400

    titlebar

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,52 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <the-titlebar/>
+    <router-view class="view"/>
   </div>
 </template>
+<script>
+import TheTitlebar from '@/components/TheTitlebar.vue';
+
+export default {
+  components: {
+    TheTitlebar,
+  },
+};
+</script>
 
 <style>
+:root {
+  --base-color: #eeeeec;
+  --bg-color: #3e4446;
+  --menu-color: #2c3133;
+  --text-color: black;
+  --dark-text-color: white;
+  --alt-text-color: grey;
+  --primary-color: #4a90d9;
+  --elevation:
+    0 2px 2px 0 rgba(0, 0, 0, 0.14),
+    0 1px 5px 0 rgba(0, 0, 0, 0.12),
+    0 3px 1px -2px rgba(0, 0, 0, 0.2);
+}
+body {
+  margin: 0;
+  padding: 0;
+  background-color: var(--bg-color);
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: Arial, Helvetica, sans-serif;
 }
-#nav {
-  padding: 30px;
+.view {
+  margin: auto;
+  margin-top: 48px;
+  padding: 10px;
+  max-width: 700px;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.card {
+  display: flex;
+  box-shadow: var(--elevation);
+  background-color: var(--base-color);
+  border-radius: 3px;
+  padding: 10px;
 }
 </style>
---
+++ src/components/TheTitlebar.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="title-bar">
+    <div class="title">GE Helper</div>
+    <div class="links" :class="{ showLinks }">
+      <router-link class="link"
+        v-for="link in links"
+        :key="link"
+        @click.native="showLinks = false"
+        :to="'/' + link">
+        {{ $t(link + '.title') }}
+      </router-link>
+    </div>
+    <div class="menu" @click="showLinks = !showLinks">
+      <svg width="24" height="24" viewBox="0 0 24 24">
+        <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" fill="var(--text-color)"/>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheTitlebar',
+  data() {
+    return {
+      links: ['home', 'module', 'hull', 'build', 'legion'],
+      showLinks: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.title-bar {
+  font-size: 18px;
+  height: 48px;
+  position: absolute;
+  top: 0; left: 0; right: 0;
+  background-color: var(--menu-color);
+  color: var(--dark-text-color);
+  display: flex;
+  justify-content: space-between;
+  align-items: stretch;
+  box-shadow: var(--elevation);
+}
+.title, .link, .menu {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+}
+.title {
+  padding: 0 12px;
+  font-size: 20px;
+  font-weight: bold;
+}
+.links {
+  display: inline-flex;
+  flex-grow: 1;
+  max-width: 600px;
+  background-color: var(--menu-color);
+}
+.link {
+  text-decoration: none;
+  flex-basis: 0;
+  flex-grow: 1;
+  color: var(--alt-text-color);
+}
+.link:hover {
+  color: var(--dark-text-color);
+}
+.router-link-active {
+  color: var(--dark-text-color);
+  box-shadow: inset 0 -4px var(--primary-color);
+}
+.menu {
+  display: none;
+  width: 48px;
+  flex-grow: 0;
+}
+@media screen and (max-width: 500px) {
+  .links {
+    display: none;
+    flex-direction: column;
+    position: fixed;
+    top: 48px; left: 0; right: 0;
+  }
+  .link {
+    height: 48px;
+    display: flex;
+  }
+  .router-link-active {
+    box-shadow: none;
+  }
+  .showLinks {
+    display: block;
+  }
+  .menu {
+    display: inline-flex;
+  }
+}
+</style>
