6e6fe433ce9f847bf076c22ed7927169c420282c
Zhangyuan Nie <zyntuz@gmail.com>
2018-10-27 12:45:32 -0400

    HullInfoTile optimizatoin

--- src/components/HullInfoTile.vue
+++ src/components/HullInfoTile.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="tile"
-    :style="style"
-    @click="$emit('selected', xycoord)">
-    <svg :width="256*zoom" :height="256*zoom" version="1.1" viewBox="0 0 256 256">
+  <div class="tile" :style="style">
+    <svg :width="svgLength" :height="svgLength" version="1.1" viewBox="0 0 256 256">
+      <text text-anchor="middle" x="128" y="128"
+        alignment-baseline="middle" style="font: 128px sans-serif">
+        {{ limit }}
+      </text>
       <path
         d="m128 5.7735 105.85 61.113v122.23l-105.85 61.113-105.85-61.113 4e-6 -122.23z"
-        fill="none" stroke="var(--text-color)" stroke-width="10"/>
+        fill="transparent" stroke="var(--text-color)" stroke-width="10"
+        @click="$emit('selected', xycoord)"/>
     </svg>
-    <div class="text" :style="textStyle">{{ limit }}</div>
   </div>
 </template>
 
@@ -29,6 +31,9 @@ export default {
     aboutHeight() {
       return (this.halfWidth * 26) / 15;
     },
+    svgLength() {
+      return 256 * this.zoom;
+    },
     style() {
       return {
         top: `${this.padding.top + (this.coord[0] * this.aboutHeight)}px`,
@@ -38,9 +43,6 @@ export default {
           (this.coord[0] % 2 ? 0 : this.halfWidth)
           }px`,
       };
-    },
-    textStyle() {
-      return { fontSize: `${180 * this.zoom}px` };
     },
     limit() {
       return this.shipLimit[this.coord[2]];
@@ -58,11 +60,5 @@ export default {
   position: absolute;
   display: inline-block;
   cursor: pointer;
-}
-.text {
-  position: absolute;
-  top: 30%;
-  left: 50%;
-  transform: translate(-50%, -30%);
 }
 </style>
